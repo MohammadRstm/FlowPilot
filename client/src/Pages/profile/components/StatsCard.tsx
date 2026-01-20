fdb8bf882b23362a81844ef979574518f63e376d
Mohamad Rostom <mohammadrstm2004@gmail.com>
2026-01-20 18:34:14 +0200

    feat(Friends): Added styling to the friends modal.

--- client/src/Pages/profile/components/StatsCard.tsx
+++ client/src/Pages/profile/components/StatsCard.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FriendsModal from "./FriendModal";
 
 type Props = {
   totals: { likes?: number; imports?: number; posts_count?: number } | { likes: number; imports: number };
