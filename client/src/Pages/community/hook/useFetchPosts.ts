7d297447796b5bcabc31352a2a298f05b54e346f
Mohamad Rostom <mohammadrstm2004@gmail.com>
2026-01-21 15:03:07 +0200

    refactor(Linting): Removed last linting error in adapters.

--- client/src/Pages/community/hook/useFetchPosts.ts
+++ client/src/Pages/community/hook/useFetchPosts.ts
@@ -5,6 +5,8 @@ import { fetchPosts } from "../../../api/community/fetchPosts";
 export type PostDto = {
   id: number;
   author: string;
+  title:string;
+  photo:string;
   username?: string | null;
   avatar?: string | null;
   content: string;
@@ -33,7 +35,7 @@ export function useFetchPosts() {
       const { current_page, last_page } = lastPage.meta;
       return current_page < last_page ? current_page + 1 : undefined;
     },
-    
+
     initialPageParam: 1,
     staleTime: 1000 * 30, // 30s
     gcTime: 1000 * 60 * 5, // 5 min
