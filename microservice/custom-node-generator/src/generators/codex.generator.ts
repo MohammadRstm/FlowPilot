3188a29b94ca347e3f4a5dc72766b4c9589be0e9
Mohamad Rostom <mohammadrstm2004@gmail.com>
2026-01-27 22:05:10 +0200

    feat(Generator): Implemented codex file generator (node.json)

---
+++ microservice/custom-node-generator/src/generators/codex.generator.ts
@@ -0,0 +1,14 @@
+import { NodeSpec } from "../types/NodeSpec";
+
+export function generateNodeJson(spec: NodeSpec) {
+  return {
+    path: `nodes/${spec.nodeName}/${spec.nodeName}.node.json`,
+    content: JSON.stringify({
+      node: `n8n-nodes-base.${spec.nodeName}`,
+      nodeVersion: "1.0",
+      codexVersion: "1.0",
+      categories: [spec.category || "Miscellaneous"],
+      resources: {}
+    }, null, 2)
+  };
+}
