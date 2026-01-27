3753a6f7dbff330ef4ed11531238ca40a53a85e4
Mohamad Rostom <mohammadrstm2004@gmail.com>
2026-01-27 22:01:47 +0200

    feat(Generator): Implemented package.json generator.

---
+++ microservice/custom-node-generator/src/generators/packageJson.generator.ts
@@ -0,0 +1,21 @@
+import { NodeSpec } from "../types/NodeSpec";
+
+export function generatePackageJson(spec: NodeSpec) {
+  return {
+    path: "package.json",
+    content: JSON.stringify({
+      name: `n8n-nodes-${spec.nodeName.toLowerCase()}`,
+      version: "0.1.0",
+      description: spec.description,
+      keywords: ["n8n-community-node-package"],
+      license: "MIT",
+      main: "index.js",
+      files: ["dist"],
+      n8n: {
+        n8nNodesApiVersion: 1,
+        credentials: [`dist/credentials/${spec.credentials.name}.credentials.js`],
+        nodes: [`dist/nodes/${spec.nodeName}/${spec.nodeName}.node.js`]
+      }
+    }, null, 2)
+  };
+}
