059db99565db5c45f9ea5019f8cfcc35eb8cc1b7
Mohamad Rostom <mohammadrstm2004@gmail.com>
2026-01-29 00:39:24 +0200

    refactor(Auth Service): Removed redundant checks.

--- server/app/Services/AuthService.php
+++ server/app/Services/AuthService.php
@@ -9,7 +9,6 @@ use Firebase\JWT\JWT;
 use Google_Client;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Http;
 
 class AuthService{
 
@@ -30,19 +29,9 @@ class AuthService{
     }
 
     public static function login(array $credentials){
+        
         $user = User::where('email', $credentials['email'])->first();
-
-        if (!$user) {
-            throw new UserFacingException("Invalid credentials");
-        }
-
-        if (!$user->password) {
-            throw new UserFacingException("This account uses Google login. Please continue with Google");
-        }
-
-        if (!Hash::check($credentials['password'], $user->password)) {
-            throw new UserFacingException("Invalid credentials");
-        }
+        self::verifyUser($user , $credentials);
 
         $token = self::createToken($user);
 
@@ -50,39 +39,24 @@ class AuthService{
     }
 
     public static function googleLogin(array $data){
-        $payload = self::verifyGoogleAccount($data);
-
-        $googleId = $payload['sub'];
-        $userData = [
-            "email" => $payload["email"],
-            "firstName" => $payload["given_name"],
-            "lastName" => $payload["family_name"]
-        ];
+        $userData = self::getUserDataFromGoogle($data);
+        $googleId = $userData["googleId"];
+        $email = $userData["email"];
 
         $user = User::where('google_id', $googleId)->first();
-        if($user && self::googleAccountAlreadyLinkedWithDifferentUser($user , $googleId)){
-            throw new UserFacingException("Google account already linked");
-        }
 
-        if(!$user){
-            $user = User::create([
-                "first_name" => $userData["firstName"],
-                "last_name" => $userData["lastName"],
-                "email" => $userData["email"],
-                "user_role_id" => env("USER_ROLE_ID"),
-                "password" => null,
-                "photo_url" => '',
-                "email_verified_at" => now()
-            ]);
-        }
+        if(!$user)  {
+            $user = User::where('email', $email)->first();// check if user exists with same email
 
-        if(!$user){
-            $isFromGoogle = 1;
-            $user = self::createUser($userData ,$isFromGoogle);
-        }
+            if($user){
+                if($user->google_id && $user->google_id !== $googleId){
+                    throw new UserFacingException("Google account already linked to another user");
+                }
 
-        if (!$user->google_id) {
-            $user->update(['google_id' => $googleId]);
+                $user->update(['google_id' => $googleId]);
+            }else{
+                $user = User::create(self::dto($userData));
+            }
         }
 
         $token = self::createToken($user);
@@ -91,11 +65,7 @@ class AuthService{
     }
 
     public static function setPassword(Model $user , array $data){
-        if($user->password){
-            if(!$data["current_password"] || !Hash::check($data["current_password"], $user->password)){
-                throw new UserFacingException("Current password is incorrect");
-            }
-        }
+        self::validatePassword($user , $data);
 
         $user->password = Hash::make($data["new_password"]);
         $user->save();
@@ -117,6 +87,63 @@ class AuthService{
     }
 
     /** helpers */
+    private static function verifyUser(User | null $user , array $credentials){
+        if(!$user){
+            throw new UserFacingException("Invalid credentials");
+        }
+
+        if(!$user->password){
+            throw new UserFacingException("This account uses Google login. Please continue with Google");
+        }
+
+        if(!Hash::check($credentials['password'], $user->password)){
+            throw new UserFacingException("Invalid credentials");
+        }
+    }
+
+    private static function getUserDataFromGoogle(array $data): array{
+            $payload = self::verifyGoogleAccount($data);
+
+            $googleId = $payload['sub'];
+            return [
+                "email" => $payload["email"],
+                "firstName" => $payload["given_name"],
+                "lastName" => $payload["family_name"],
+                "googleId" => $googleId
+            ];
+    }
+
+    private static function dto(array $data): array{
+        return[
+                "first_name" => $data["firstName"],
+                "last_name" => $data["lastName"],
+                "email" => $data["email"],
+                "google_id" => $data["googleId"],
+                "user_role_id" => env("USER_ROLE_ID"),
+                "password" => null,
+                "photo_url" => '',
+                "email_verified_at" => now()
+            ];
+    }
+
+    private static function validatePassword(User $user , array $data){
+        if($user->password){
+            if(!$data["current_password"] || !Hash::check($data["current_password"], $user->password)){
+                throw new UserFacingException("Current password is incorrect");
+            }
+        }
+
+        // validate if password doesn't contain a capital letter
+        if(!preg_match('/[A-Z]/', $data["new_password"])){
+            throw new UserFacingException("Password must contain at least one capital letter"); 
+        }
+
+        // validate if password length is at least 8 characters
+        if(strlen($data["new_password"]) < 8){
+            throw new UserFacingException("Password must be at least 8 characters long");
+        }
+    }
+
     private static function verifyGoogleAccount(array $data){
         $client = new Google_Client([
             'client_id' => env('GOOGLE_CLIENT_ID'),
@@ -170,15 +197,5 @@ class AuthService{
         }
 
         return $secret;
-    }
-
-    private static function googleAccountAlreadyLinkedWithDifferentUser(Model $user , string | int $googleId){
-        return User::where('google_id', $googleId)
-                ->where('id', '!=', optional($user)->id)
-                ->exists();
-    }
-
-    private static function getUserByEmail(array $userData){
-        return User::where('email', $userData["email"])->first();
     }
 }
