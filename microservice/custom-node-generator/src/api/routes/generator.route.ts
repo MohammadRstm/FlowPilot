9bd5e01401ab695043eca978055eb51ce2bca045
Mohamad Rostom <mohammadrstm2004@gmail.com>
2026-02-02 23:26:58 +0200

    feat(Validation): Hooked generator routes to validation middlware.

--- microservice/custom-node-generator/src/api/routes/generator.route.ts
+++ microservice/custom-node-generator/src/api/routes/generator.route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-
-
+import { validate } from "../middlwares/validate.middlware";
+import { generatorSchema } from "../validation/generator.validation";
 
 const generateCustomNodeRoutes = Router();
 
-generateCustomNodeRoutes.post("/" , generateCustomNodeRoutes);
+generateCustomNodeRoutes.post("/" , validate(generatorSchema) ,  generateCustomNodeRoutes);
 
 export default generateCustomNodeRoutes;
